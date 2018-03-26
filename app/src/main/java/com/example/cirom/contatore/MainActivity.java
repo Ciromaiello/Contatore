36f35074b87afbcac71cc3c5af334e4d431ffaeb
Ciro Maiello <ciro.mai@hotmail.it>
2018-03-26 16:17:58 +0200

    Azzeramento e raddoppio completati

--- app/src/main/java/com/example/cirom/contatore/MainActivity.java
+++ app/src/main/java/com/example/cirom/contatore/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView vMessaggio;
     private Button vAumenta;
     private Button vDiminuisci;
+    private Button vAzzera;
+    private CheckBox vRaddoppio;
     private int cont = 0;
 
     @Override
@@ -22,11 +25,15 @@ public class MainActivity extends AppCompatActivity {
         vMessaggio = findViewById(R.id.textMessaggio);
         vAumenta = findViewById(R.id.btnAumenta);
         vDiminuisci = findViewById(R.id.btnDiminuisci);
+        vAzzera = findViewById(R.id.btnAzzera);
+        vRaddoppio = findViewById(R.id.chkRaddoppia);
 
         vAumenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ++cont;
+                if (vRaddoppio.isChecked())
+                    ++cont;
                 mostraMessaggio();
             }
         });
@@ -35,9 +42,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 --cont;
+                if (vRaddoppio.isChecked())
+                    --cont;
                 mostraMessaggio();
             }
         });
+
+        vAzzera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cont=0;
+                mostraMessaggio();
+            }
+        });
+
     }
 
     /**
