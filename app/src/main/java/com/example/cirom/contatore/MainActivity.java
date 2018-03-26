d3ccecc2feb9941522782a8308254b57587e6284
Ciro Maiello <ciro.mai@hotmail.it>
2018-03-26 15:50:38 +0200

    Prima versione funzionante

--- app/src/main/java/com/example/cirom/contatore/MainActivity.java
+++ app/src/main/java/com/example/cirom/contatore/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.cirom.contatore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // View del layout
+    private TextView vMessaggio;
+    private Button vAumenta;
+    private Button vDiminuisci;
+    private int cont = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Imposto riferimenti delle view
+        vMessaggio = findViewById(R.id.textMessaggio);
+        vAumenta = findViewById(R.id.btnAumenta);
+        vDiminuisci = findViewById(R.id.btnDiminuisci);
+
+        vAumenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ++cont;
+                mostraMessaggio();
+            }
+        });
+
+        vDiminuisci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                --cont;
+                mostraMessaggio();
+            }
+        });
+    }
+
+    /**
+     * Funzione che visualizza il contatore
+     */
+    private void mostraMessaggio(){
+        vMessaggio.setText(getString(R.string.messaggio2) + " " + cont); // Prende il messaggio dalle risorse
     }
 }
